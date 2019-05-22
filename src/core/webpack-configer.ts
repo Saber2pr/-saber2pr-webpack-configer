1db46d8d865e9848e198ae1990c72b7d002e4574
saber2pr <saber2pr@gmail.com>
2019-05-22 21:32:31 +0000

    feat(*): init project

---
+++ src/core/webpack-configer.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: saber2pr
+ * @Date: 2019-05-22 20:47:19
+ * @Last Modified by: saber2pr
+ * @Last Modified time: 2019-05-22 21:01:32
+ */
+import webpack from 'webpack'
+
+export function WebpackConfig(config: webpack.Configuration) {
+  return config
+}
---
+++ src/test/test.ts
@@ -0,0 +1,32 @@
+import { WebpackConfig } from '../core/webpack-configer'
+
+export const config = WebpackConfig({
+  mode: 'production',
+  entry: './src/index.ts',
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  output: {
+    filename: 'bundle-[hash].js',
+    path: `/build/bundle-[hash].js`
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader']
+      },
+      {
+        test: /\.less$/,
+        use: []
+      }
+    ]
+  }
+})
+
+console.log(config)
